27370dc72eef970b38d83834ded557ce204c4d20
MeganTai <megan.tai@ugent.be>
2022-04-27 14:08:59 +0200

    lijst maken self.potten

--- kamers.py
+++ kamers.py
@@ -17,14 +17,7 @@ class Kamers:
         # (x-pos, y-pos, breedte, hoogte, afbeelding_naam)  
 
         afbeeldingen_folder = pathlib.Path("afbeeldingen")
-        self.potten = [Items(145,207,36,36, afbeeldingen_folder / "pot.PNG"), Items(145,303,36,36, afbeeldingen_folder / "pot.PNG")]
-        self.pot_1 = Items(145,207,36,36, afbeeldingen_folder / "pot.PNG")
-        self.pot_2 = Items(145,303,36,36, afbeeldingen_folder / "pot.PNG")
-        self.pot_3 = Items(145,399,36,36, afbeeldingen_folder / "pot.PNG")
-        self.pot_4 = Items(145,495,36,36, afbeeldingen_folder / "pot.PNG")
-        self.pot_5 = Items(290,303,36,36, afbeeldingen_folder / "pot.PNG")
-        self.pot_6 = Items(290,399,36,36, afbeeldingen_folder / "pot.PNG")
-        self.pot_7 = Items(386,495,36,36, afbeeldingen_folder / "pot.PNG")
+        self.potten = [Items(145,207,36,36, afbeeldingen_folder / "pot.PNG"), Items(145,303,36,36, afbeeldingen_folder / "pot.PNG"), Items(145,399,36,36, afbeeldingen_folder / "pot.PNG"), Items(145,495,36,36, afbeeldingen_folder / "pot.PNG"),Items(290,303,36,36, afbeeldingen_folder / "pot.PNG"), Items(290,399,36,36, afbeeldingen_folder / "pot.PNG"),Items(386,495,36,36, afbeeldingen_folder / "pot.PNG")]
         self.pc = Items(199,197,51,34, afbeeldingen_folder / "pc.png")
         self.boek = Items(241,200,18,21, afbeeldingen_folder / "eboek.PNG")
         self.portret = Items(409,112,94,47, afbeeldingen_folder / "portret.PNG")
@@ -32,7 +25,7 @@ class Kamers:
         self.vuilbak = Items(580, 510, 60, 60, afbeeldingen_folder / "vuilbak.PNG")
         self.kast = Items(98,183,48,96, afbeeldingen_folder / "kast.PNG")
         
-        self.bureau_sprites.add(self.pot_1, self.pot_2, self.pot_3, self.pot_4, self.pot_5, self.pot_6, self.pot_7, self.pc, self.boek, self.portret, self.hamer, self.vuilbak, self.kast)
+        self.bureau_sprites.add(self.potten, self.pc, self.boek, self.portret, self.hamer, self.vuilbak, self.kast)
 
     def grow(self,sprite):
         Items.scale(self,sprite)
