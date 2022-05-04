6cc1f510c3fdd8820a44df98b6b962a784c6895a
Thomas <thomas.heymans@ugent.be>
2022-05-04 15:05:20 +0200

    test

--- input.py
+++ input.py
@@ -9,6 +9,7 @@ class Input:
         self.tekst = tekst
         self.tekst_print = FONT.render(text, True, self.color)
         self.actief = False
+        nvbnvvn
     
     def :
         if event.type
