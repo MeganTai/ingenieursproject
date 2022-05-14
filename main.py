390dd3bc4a61aa6ef5391848fc1faa8ce9b3f293
Dario <dario.vervliet@ugent.be>
2022-05-14 13:28:55 +0200

    klikdetectie probleem niet opgelost

--- main.py
+++ main.py
@@ -11,28 +11,33 @@ FPS = pygame.time.Clock()
 
 world = World()
 
-pygame.display.set_caption("escape_room versie 1")
+pygame.display.set_caption("Escape-it")
 World = pygame.display.set_mode((646, 605))
 while True:
     events = pygame.event.get()
     FPS.tick(30)
 
     world.act()
-
+    
     pygame.display.update()
     for event in events:
+        print(event)
         if event.type == QUIT:
             pygame.quit() 
             sys.exit()
-        if event.type == pygame.MOUSEMOTION:
-            world.mouse_action()
-        if event.type == pygame.MOUSEBUTTONDOWN:
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
             world.mouse_click()
-        if event.type == pygame.KEYDOWN:
+
+        elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 world.space_bar()
             if event.key == pygame.K_f:
                 if world.background == world.eindgame.background:
                     Eindgame.deselect(world.eindgame)
+        
+        elif event.type == pygame.MOUSEMOTION:
+            world.mouse_action()
+        
             
         
